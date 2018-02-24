c2c5b838cb257ac5d3067f00e65efdd5977f5846
Joel Martinez <jmarti221@gmail.com>
2018-02-24 14:09:13 -0700

    Initial websensus commit

---
+++ websensus/java/com/websensus/frontend/MainServer.java
@@ -0,0 +1,7 @@
+package com.websensus.frontend;
+
+class MainServer {
+    public static void main(String[] args) {
+        System.out.println("HI WORLD!");
+    }
+}
---
+++ websensus/java/com/websensus/reviews/ReviewGenerator.java
@@ -0,0 +1,4 @@
+package com.websensus.reviews;
+
+public class ReviewGenerator {
+}
